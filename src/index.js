d84a7bf368a1a6f600d1939748e0d91cc12cb2b4
ysz951 <shengyang_zhou@baylor.edu>
2020-08-09 06:16:27 -0500

    delete useless icon

--- src/index.js
+++ src/index.js
@@ -4,17 +4,9 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faFrog, faDrumstickBite, faListUl, faListOl, faPenAlt, faGlobeAmericas, faBookOpen, faComment, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import { faDrumstickBite } from '@fortawesome/free-solid-svg-icons'
 import { RecipeListProvider } from './context/RecipeListContext'
 library.add(
-  faFrog,
-  faListUl, // style: listicle
-  faListOl, // style: howto
-  faGlobeAmericas, // style: news
-  faPenAlt, // style: interview
-  faBookOpen, // style: story
-  faComment,
-  faQuoteLeft,
   faDrumstickBite
 );
 
