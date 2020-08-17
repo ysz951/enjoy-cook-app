7f89c7d52a71b8b784cd227c1978fe7ce2540a28
ysz951 <shengyang_zhou@baylor.edu>
2020-08-17 04:49:29 -0500

    change config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "https://lit-mesa-65228.herokuapp.com/api",
-    TOKEN_KEY: 'enjoy-cook-auth-token',
-    // TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "https://dry-reef-45342.herokuapp.com/api",
+    TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY
 }
